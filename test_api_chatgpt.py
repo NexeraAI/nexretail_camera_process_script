cc99fe07699e68fa2f89c3ff094a7f9a64e5b500
William Hsu <williamhsu17@gmail.com>
2024-11-19 12:28:49 +0800

    chatgpt api test

---
+++ test_api_chatgpt.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+
+client = OpenAI(
+  organization='',
+  project='',
+  api_key='',
+)
+
+completion = client.chat.completions.create(
+  model="gpt-4o",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Hello!"}
+  ]
+)
+
+print(completion.choices[0].message)
